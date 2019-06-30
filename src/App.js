ee104a0749d513d47cc7d4b50eab6c74d67edbc1
ShandelleLeigh <ShandelleLovato@gmail.com>
2019-06-29 23:21:24 -0600

    Consolidate code

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import TopMenu from './components/TopMenu';
 import GameTable from './components/GameTable'
 import theme from './styles';
 
-// const GameDeck = (deck) => {
-//   let cardDeck = deck.deck
-//   console.log('cardDeck:', deck, 'typeof:', typeof deck, 'length:', deck.deck.length)
-//   return (
-//     <div>
-//       Game deck
-//       {
-//         (cardDeck.length > 1) ?
-//         cardDeck.forEach( card => {
-//           const {cardNo, name, value, suit, color} = card
-//           console.log('card No: ', cardNo)
-//           return (
-//             <div>
-//                 card number:
-//               {card.cardNo} {name} {value} {suit} {color}
-//             </div>
-//           )
-//         })
-//         : null
-//       }
-//     </div>
-//   )
-// }
-
 function App() {
   const [gameState, setGameState] = useState({cardDeck: {}})
   const startNewGame = () => {
@@ -44,7 +20,7 @@ function App() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <ThemeProvider  theme={theme} >
         <TopMenu startNewGame={startNewGame}/>
-        <GameTable game={gameState.cardDeck} setGameState={setGameState}/>
+        <GameTable game={gameState} setGameState={setGameState}/>
       </ThemeProvider>
     </div>
   );
--- src/components/GameTable.js
+++ src/components/GameTable.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Card} from '@material-ui/core';
-// import theme from './styles';
 
 const useStyles = makeStyles(
   theme => ({
     base: {
       margin: `0 ${theme.spacing(4)}px`,
       height: `calc(100% - ${theme.spacing(8)}px)`,
-      // backgroundColor: '#4c954c',
       backgroundColor: theme.palette.secondary.main,
     },
     card: {
@@ -20,43 +17,35 @@ const useStyles = makeStyles(
 );
 
 const GameDeck = ({deck, classes}) => {
-  console.log('game table, ln 24, cardDeck:', deck, 'typeof:', typeof deck, 'length:', deck.length)
+  // console.log('game table, ln 24, cardDeck:', deck, 'typeof:', typeof deck, 'length:', deck.length)
   return (
     <div>
-      Game deck
-      <div>{
+      {
         (deck.length > 1) ?
-        deck.map( card => {
-          const {cardNo, name, value, suit, color} = card
-          console.log('card No: ', cardNo)
-          return (
-            <div className={classes.card}>
-                {name} of {suit}
-            </div>)
-
+          deck.map(card => {
+            const { name, suit, /* cardNo,  value,  color */ } = card
+            return (
+              <div className={classes.card} key={`${name}-${suit}`}>
+                  {name} of {suit}
+              </div>
+            )
         })
         : null
       }
-       </div>
     </div>
   )
 }
 
-const GameTable = (deck) => {
-  console.log('game table ln 47: deck: ', deck.game)
+const GameTable = ({
+  game,
+  _ // TODO: setGameState()
+}) => {
+  const {cardDeck} = game
   const classes = useStyles();
-
   return (
     <>
-      <div
-        className={classes.base}
-      >
-
-      Game Table
-
-      {/* TODO: Game has state of cards */}
-      {/* TODO: Generate Cards */}
-        <GameDeck deck={deck.game} classes={classes}/>
+      <div className={classes.base} >
+        <GameDeck deck={cardDeck} classes={classes}/>
       </div>
     </>
   )
